413ce4eb690d1c813f6a2f4e28ab17e7b4a9787a
EpicCoconutGuy <mjay.jirathanat@gmail.com>
2024-05-14 18:23:56 +0700

    add some stuff

--- backend.py
+++ backend.py
@@ -1,3 +1,19 @@
 import streamlit as st
-import openai as openai
+import openai
+import os
+from dotenv import load_dotenv
+
+class chatGPT:
+    def __init__(self):
+        load_dotenv()
+        self.api_key = os.getenv("openai_APIKEY")
+
+    def generate_response(self, prompt, model="gpt-3.5-turbo-0125"):
+        openai.api_key = self.api_key
+        response = openai.completions.create(
+            model=model,
+            prompt=prompt
+        )
+        return response.choices[0].message.content
+
 
---
+++ home.py
@@ -0,0 +1,4 @@
+from backend import chatGPT
+import streamlit
+
+streamlit.write("testing")
