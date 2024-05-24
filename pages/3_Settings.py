ae9c61c730a08f11a021eeb22898c325009d5ce7
EpicCoconutGuy <mjay.jirathanat@gmail.com>
2024-05-24 13:37:57 +0700

    deploy test2

--- pages/3_Settings.py
+++ pages/3_Settings.py
@@ -10,8 +10,8 @@ if "appearance" not in st.session_state:
      st.session_state["appearance"] = "Dark"
 
 with st.form("settings_form"):
-     noti = st.toggle(label="Recieve Notification", value=st.session_state["notification"], key="notification")
-     sound = st.slider("Volume", min_value=0, max_value=100, value=st.session_state["sound"], key="sound")
+     noti = st.toggle(label="Recieve Notification", key="notification")
+     sound = st.slider("Volume", min_value=0, max_value=100, key="sound")
      appearance = st.selectbox("Appearance", ["Dark", "Light"] ,index=0 if st.session_state["appearance"] == "Dark" else 1, key="appearance")
      submit = st.form_submit_button("Save Change")
      
